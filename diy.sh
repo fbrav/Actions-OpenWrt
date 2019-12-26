f91e98d4e320e01be6d00687c7fb17ef93d88e28
fbrav <58653349+fbrav@users.noreply.github.com>
2019-12-26 16:47:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.168.1/g' package/base-files/files/bin/config_generate
+#sed -i "s/PKG_VERSION:=0.30.0/PKG_VERSION:=0.27.0/g" package/lean/frpc/Makefile
